c0e98212cf776dabd308ad92597aacf0d9702b5f
Nokmer <113694239+Nokmer@users.noreply.github.com>
2022-12-10 21:32:57 +0200

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-def sleight_of_hand(choice: int, field: str) -> int:
-    res = 0
-    for i in range(1, 10):
-        repetition = field.count(str(i))
-        if repetition <= 2*choice and repetition > 0:
-            res += 1
-    return res
